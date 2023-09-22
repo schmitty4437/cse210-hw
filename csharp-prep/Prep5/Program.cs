0884b672e6a079f5dd621da4cb6d95a43e4faba2
Matt Schmitt <matt@renewable-innovations.com>
2023-09-21 20:15:48 -0600

    Prep #5 complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared = SquareNumber(number);
+
+        DisplayResult(name, squared);
+
+        //Function to display welcome message
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        //Function to prompt user for name
+        static string PromptUserName()
+        {
+            Console.Write("What is your name: ");
+            string userName = Console.ReadLine();
+
+            return userName;
+        }
+
+        //Function to prompt user for their favorite number
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number: ");
+            string temp = Console.ReadLine();
+            int userNumber = int.Parse(temp);
+
+            return userNumber;
+        }
+
+        //Function that calculates their number squared
+        static int  SquareNumber(int squareNumber)
+        {
+            int x = squareNumber * squareNumber;
+
+            return x;
+        }
+
+        //Function that displays users number and squared number
+        static void DisplayResult(string userName, int squareNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your numer is {squareNumber}");
+        }
     }
 }
