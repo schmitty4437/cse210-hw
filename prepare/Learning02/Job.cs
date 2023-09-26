11974cb78344d76365c16bd77c6099c28496a39a
Matt Schmitt <schmitty4437@gmail.com>
2023-09-26 09:52:30 -0600

    Completed Learning #2

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,25 @@
+using System;
+
+// Class: Job
+// Attributes:
+// * _company : string
+// * _jobTitle : string
+// * _startYear : int
+// * _endYear : int
+
+// Behaviors:
+// * Display() : void
+
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
